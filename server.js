3c1078b3988b09e424752a5f6098fe9c1d5409ab
VinSe <vincentmcmaps@gmail.com>
2022-01-21 19:45:07 +0100

    Added edit functionality

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/IPRWC_Webshop'));
+app.use(express.static('./dist/iprwc-webshop'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/IPRWC_Webshop/'}),
+  res.sendFile('index.html', {root: 'dist/iprwc-webshop/'}),
 );
 
 // Start the app by listening on the default Heroku port
